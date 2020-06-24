23788029c69cefafbcefaabc49f7911889eb73f1
Byadiso <byadiso@gmail.com>
2020-06-24 19:40:50 +0200

    add style

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,14 +1,23 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,    
-)
-FilePond.setOptions({
-    stylePanelAspectRatio: 150/100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150,
+const rootStyles = window.getComputedStyle(document.documentElement)
+if (rootStyles.getPropertyValue('--book-cover-width-large') != null ){
+    ready()
+}
+
+
+function ready(){
+
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,    
+    )
+    FilePond.setOptions({
+        stylePanelAspectRatio: 150/100,
+        imageResizeTargetWidth: 100,
+        imageResizeTargetHeight: 150,
+        
+    })
     
-})
-
-
-FilePond.parse(document.body);
+    
+    FilePond.parse(document.body);
+}
